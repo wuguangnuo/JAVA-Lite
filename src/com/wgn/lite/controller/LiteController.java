310ebec0cb7afd29d4053033ee4d7bd5618a37cd
吴广诺 <wuguangnuo@qq.com>
2019-04-02 18:21:01 +0800

    28

--- src/com/wgn/lite/controller/LiteController.java
+++ src/com/wgn/lite/controller/LiteController.java
@@ -611,4 +611,40 @@ public class LiteController {
         }
         return true;
     }
+
+    /**
+     * 28. 实现strStr()
+     */
+    private int strStr(String haystack, String needle) {
+//        输入: haystack = "hello", needle = "ll"
+//        输出: 2
+
+        char[] source = haystack.toCharArray();
+        char[] target = needle.toCharArray();
+        int targetCount = target.length;
+
+        if (targetCount == 0) return 0;
+
+        char first = target[0];
+        int max = source.length - targetCount;
+
+        for (int i = 0; i <= max; i++) {
+            /* Look for first character. */
+            if (source[i] != first) {
+                while (++i <= max && source[i] != first) ;
+            }
+
+            /* Found first character, now look at the rest of v2 */
+            if (i <= max) {
+                int j = i + 1;
+                int end = j + targetCount - 1;
+                for (int k = 1; j < end && source[j] == target[k]; j++, k++) ;
+                if (j == end) {
+                    /* Found whole string. */
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
 }
