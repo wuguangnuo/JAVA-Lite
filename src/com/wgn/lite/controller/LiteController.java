d878f38a308de5da21fcf2ec4504bfa51b58e94d
吴广诺 <wuguangnuo@qq.com>
2019-07-17 18:39:40 +0800

    215

--- src/com/wgn/lite/controller/LiteController.java
+++ src/com/wgn/lite/controller/LiteController.java
@@ -1689,4 +1689,23 @@ public class LiteController {
         }
         return 0;
     }
+
+    /**
+     * 215.数组中的第K个最大元素
+     */
+    public int findKthLargest(int[] nums, int k) {
+        // init heap 'the smallest element first'
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        // keep k largest elements in the heap
+        for (int n : nums) {
+            heap.add(n);
+            if (heap.size() > k)
+                heap.poll();
+        }
+
+        // output
+        return heap.poll();
+    }
 }
